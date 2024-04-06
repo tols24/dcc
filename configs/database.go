f6e458dba4cc1bb8e825183d9f619704ba0e4be0
tols24 <toardaniellapian24@gmail.com>
2024-04-06 18:17:38 +0700

    feat:database

--- configs/database.go
+++ configs/database.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "root:password@tcp(localhost:3306)/asg2?parseTime=true"
+	dsn := "root:password@tcp(database-1.cvm68gew0feu.us-east-1.rds.amazonaws.com:3306)/asg2?parseTime=true"
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
